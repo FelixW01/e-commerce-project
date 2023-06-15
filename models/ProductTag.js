06255a1c564199eac093066643bb631a7d7e1e4f
Felix W <felixwillem01@yahoo.com>
2023-06-15 17:12:31 -0400

    Added sequelize models and relationship index.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,33 @@
-const { Model, DataTypes } = require('sequelize');
+const {
+  Model,
+  DataTypes
+} = require('sequelize');
 
 const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
-    // define columns
+ProductTag.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
+  //references product.id as foreign key
+  product_id: {
+    type: DataTypes.INTEGER,
+  },
+  //references tag.id as foreign key
+  tag_id: {
+    type: DataTypes.INTEGER,
   }
-);
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'product_tag',
+});
 
 module.exports = ProductTag;
