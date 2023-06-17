b3455aa8835f097f2c48b886c7346dc0046deae4
Felix W <felixwillem01@yahoo.com>
2023-06-16 22:58:58 -0400

    finished delete routes on category, product, and tag

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -68,8 +68,23 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deletedCategory = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!deletedCategory) {
+      res.status(404).json({
+        message: "Can't find category with that id!"
+      })
+    }
+    res.status(200).json(deletedCategory);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -95,8 +95,23 @@ router.put('/:id', (req, res) => {
 
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deletedProduct = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!deletedProduct) {
+      res.status(404).json({
+        message: "Can't find product with that id!"
+      })
+    }
+    res.status(200).json(deletedProduct);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
